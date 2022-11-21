ffa0d5b367daf7713869780b033f552e44af545e
IvanBogdantsev <bogdantsev.st@gmail.com>
2022-11-21 17:30:35 +0100

    Created template

---
+++ Parilet/Constants/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Parilet
+//
+//  Created by Vanya Bogdantsev on 21.11.2022.
+//
+
+import Foundation
+
+struct MapBoxConstants {
+    static let accesToken = "pk.eyJ1IjoieXZhbmIiLCJhIjoiY2xhcXl6eTg5MDFrdzQwb3Z6OTAxeG9vcyJ9.f9bFprti9yDDWP1MigBL9Q"
+}
---
+++ Parilet/ViewController/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Parilet
+//
+//  Created by Vanya Bogdantsev on 21.11.2022.
+//
+
+import UIKit
+import MapboxMaps
+import RxSwift
+ 
+class ViewController: UIViewController {
+    
+    private var mapView: MapView!
+    
+    override public func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpMapView()
+    }
+    
+    private func setUpMapView() {
+        let myResourceOptions = ResourceOptions(accessToken: MapBoxConstants.accesToken)
+        let myMapInitOptions = MapInitOptions(resourceOptions: myResourceOptions)
+        mapView = MapView(frame: view.bounds, mapInitOptions: myMapInitOptions)
+        mapView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        self.view.addSubview(mapView)
+    }
+    
+}
