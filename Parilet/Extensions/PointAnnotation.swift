5fb56c466baca1be6d276e57e283c13865f9a4f7
IvanBogdantsev <bogdantsev.st@gmail.com>
2022-11-27 21:27:41 +0100

    map annotations

---
+++ Parilet/Extensions/PointAnnotation.swift
@@ -0,0 +1,8 @@
+//
+//  PointAnnotation.swift
+//  Parilet
+//
+//  Created by Vanya Bogdantsev on 27.11.2022.
+//
+
+import Foundation
---
+++ Parilet/Extensions/UIImage.swift
@@ -0,0 +1,8 @@
+//
+//  UIImage.swift
+//  Parilet
+//
+//  Created by Vanya Bogdantsev on 27.11.2022.
+//
+
+import Foundation
