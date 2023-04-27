ed7b2b2b6263ce299b1b29eab7830c5a7870f56e
IvanBogdantsev <bogdantsev.st@gmail.com>
2023-04-27 20:24:20 +0200

    Project renamed

--- Toilet Radar/Application/AppDelegate.swift
+++ Toilet Radar/Application/AppDelegate.swift
@@ -12,7 +12,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication,
                      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        Thread.sleep(forTimeInterval: 5)
         return true
     }
 
